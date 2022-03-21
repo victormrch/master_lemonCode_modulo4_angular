eefb840e1ef273ffca214b1c0789630d0137aa3a
“Victor <victor.raman@eu.jll.com>
2022-03-21 20:18:32 +0100

    first commit

---
+++ src/api/mock-data.gallery.ts
@@ -0,0 +1,44 @@
+import { ApiPictureEntity } from './api.model';
+
+export const apiPictureList: ApiPictureEntity[] = [
+  {
+    id: '4kk33',
+    title: 'vader',
+    src: 'assets/gallery/fondo_1.jpeg',
+  },
+  {
+    id: '5rt329',
+    title: 'trooper',
+    src: 'assets/gallery/fondo_2.jpeg',
+  },
+  {
+    id: 'jd782j',
+    title: 'rey_final',
+    src: 'assets/gallery/fondo_3.jpeg',
+  },
+  {
+    id: 'kd82j',
+    title: 'despertar',
+    src: 'assets/gallery/fondo_4.jpeg',
+  },
+  {
+    id: '0lokw2',
+    title: 'vader_cabeza',
+    src: 'assets/gallery/fondo_5.jpeg',
+  },
+  {
+    id: 'd83j2u',
+    title: 'new_trooper',
+    src: 'assets/gallery/fondo_6.jpeg',
+  },
+  {
+    id: 'idueh43',
+    title: 'amenaza',
+    src: 'assets/gallery/fondo_7.jpeg',
+  },
+  {
+    id: '0i7gx1',
+    title: 'trooper_shooting',
+    src: 'assets/gallery/fondo_8.jpeg',
+  },
+];
